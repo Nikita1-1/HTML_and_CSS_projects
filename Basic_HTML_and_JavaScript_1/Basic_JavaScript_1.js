16a80232620f2aef8b2e5c07a8e9df4b08dd533f
SazonovN <109035573+SazonovN@users.noreply.github.com>
2022-08-25 19:33:31 -0600

    commit

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+
+function My_First_JS_Function() {
+    var String = "kiss Me, I am Irish";
+    var result = String.fontcolor("green")
+    document.getElementById("Irish").innerHTML =
+    result;
+}
---
+++ Basic_HTML_and_JavaScript_1/javascrip.js
@@ -0,0 +1,8 @@
+alert('Hello World');
+
+function My_First_Function() {
+    var String = "kiss Me, I am Irish";
+    var result = String.fontcolor("green")
+    document.getElementById("Irish").innerHTML =
+    result;
+}
